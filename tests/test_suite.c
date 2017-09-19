c9fe96eed75494b68aca99a543ac3f0387fe4774
Zack Brown <zbrown@pillartechnology.com>
2017-09-19 15:07:03 -0400

    free allocated stack memory

--- tests/test_suite.c
+++ tests/test_suite.c
@@ -6,8 +6,8 @@
 
 START_TEST(it_allocates_a_single_byte_and_stores_0xFF)
 {
-    s_allocator_t *stack; 
-    char * s; 
+    s_allocator_t *stack;
+    char * s;
 
     stack = s_alloc_init(1);
     // 8-bit character can be aligned to any address
@@ -37,6 +37,37 @@ START_TEST(it_makes_multiple_allocations)
 }
 END_TEST
 
+START_TEST(it_rolls_back_and_reallocates)
+{
+    s_allocator_t *stack;
+    char *a;
+    char *b;
+    char *c;
+    char *d;
+
+    stack = s_alloc_init(2);
+    a = s_alloc(stack, 1, 0);
+    b = s_alloc(stack, 1, 0);
+
+    memset(a, 0x00, 1);
+    memset(b, 0x01, 1);
+
+    // free
+    s_free(stack, a);
+
+    c = s_alloc(stack, 1, 0);
+    d = s_alloc(stack, 1, 0);
+
+    memset(c, 0x02, 1);
+    memset(d, 0x03, 1);
+
+    ck_assert_int_eq(*((unsigned char *)c), 0x02);
+    ck_assert_int_eq(*((unsigned char *)d), 0x03);
+    ck_assert_int_eq(*((unsigned char *)a), *((unsigned char *)c));
+    ck_assert_int_eq(*((unsigned char *)b), *((unsigned char *)d));
+}
+END_TEST
+
 Suite *make_memory_allocator_unit_test_suite()
 {
     Suite *s;
@@ -47,6 +78,7 @@ Suite *make_memory_allocator_unit_test_suite()
 
     tcase_add_test(tc, it_allocates_a_single_byte_and_stores_0xFF);
     tcase_add_test(tc, it_makes_multiple_allocations);
+    tcase_add_test(tc, it_rolls_back_and_reallocates);
 
     suite_add_tcase(s, tc);
 
